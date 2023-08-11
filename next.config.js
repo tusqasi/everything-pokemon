6cd4168850f7ab0ab6f7521a909e146375e425fe
tusqasi <2002.tushar.kuntawar@gmail.com>
2023-08-11 15:24:29 +0530

    🔧 Adding more config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	dev:false,
+
 	reactStrictMode: true,
 	images: {
 		domains: ["*"],
--- src/pages/api/findname.js
+++ src/pages/api/findname.js
@@ -1,3 +1,4 @@
+// const sqlite3 = require('sqlite3').verbose();
 import sqlite3 from "sqlite3";
 import { open, Database } from "sqlite";
 
@@ -5,9 +6,10 @@ let db = null;
 
 export default async function handler(req, res) {
 	if (!db) {
+		// If the database instance is not initialized, open the database connection
 		db = await open({
-			filename: "/static/poke.db",
-			driver: sqlite3.Database,
+			filename: "static/poke.db", // Specify the database file path
+			driver: sqlite3.Database, // Specify the database driver (sqlite3 in this case),
 		});
 	}
 	const partial_name = req.body.partial_name;
@@ -15,7 +17,6 @@ export default async function handler(req, res) {
 	const query = `select * from pokemon_stats where name like '%${partial_name}%'`;
 
 	const found = await db.all(query);
-	console.log(found);
 	res.status(200).json({
 		products: found.slice(0, limit)
 	});
